7e727bd872bdebad7fcc9bcb04b91568d1666f2e
Joao Paulo de Souza <vibedigital@gmail.com>
2014-11-18 12:30:56 -0200

    minor changes

--- cadastro_extended/cadastro_extended.py
+++ cadastro_extended/cadastro_extended.py
@@ -5,13 +5,7 @@ class cadastro_cadastro(osv.osv):
     _name = 'cadastro.cadastro'
     _inherit = 'cadastro.cadastro'
 
-    def fnct(self, cr, uid, ids, fields, arg, context):
-        x = {}
 
-        for record in self.browse(cr, uid, ids):
-            x[record.id] = record.valor1 + record.valor2
-        return x
-    
     _columns = {
     'novo_campo':fields.char('novo campo',size=16,required = True,translate = True),
     }
